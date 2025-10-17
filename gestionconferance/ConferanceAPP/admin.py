4d62ba5f1b3abd8dd1df1ee1729a24561c5c2311
karimrebai123 <karim.rebai@esprit.tn>
2025-10-17 17:51:13 +0100

    third commit avec la personalisation des modeles

--- gestionconferance/ConferanceAPP/admin.py
+++ gestionconferance/ConferanceAPP/admin.py
@@ -5,7 +5,7 @@ admin.site.site_title="Gestion Conference 25/26"
 admin.site.site_header="Gestion conferences"
 admin.site.index_title="django App conference"
 #admin.site.register(Conference)
-admin.site.register(Submission)
+#admin.site.register(Submission)
 admin.site.register(organizing_committe)
 
 class SubmissionInline(admin.StackedInline):
@@ -37,3 +37,33 @@ class AdminConferenceModel(admin.ModelAdmin):
         return "rien a signaler"
     duration.short_description="Duration (days)"
     inlines=[SubmissionInline]
+    
+
+@admin.register(Submission)
+class AdminSubmission(admin.ModelAdmin):
+    # Colonnes affichées dans la liste
+    list_display = ("title", "status", "user", "conference", "submission_date", "payed")
+    
+    # Rendre certaines colonnes modifiables directement depuis la liste
+    list_editable = ("status", "payed")
+    
+    # Filtres et recherche
+    list_filter = ("status", "payed", "conference", "submission_date")
+    search_fields = ("title", "keywords", "user__username")
+    
+    # Champs en lecture seule
+    readonly_fields = ("submission_id", "submission_date")
+    
+    # Organisation des champs dans le formulaire
+    fieldsets = (
+        ("Infos générales", {"fields": ("submission_id", "title", "abstract", "keywords")}),
+        ("Fichier et conférence", {"fields": ("paper", "conference")}),
+        ("Suivi", {"fields": ("status", "payed", "submission_date", "user")}),
+    )
+
+    # Action simple pour marquer comme payée
+    actions = ["mark_as_payed"]
+
+    def mark_as_payed(self, request, queryset):
+        queryset.update(payed=True)
+    mark_as_payed.short_description = "Marquer comme payées"
